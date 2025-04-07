a926ede28e47ed41f02e2147f3c6a18d0ee595be
maajidz <maajidz@yahoo.com>
2025-04-07 21:06:51 +0530

    Add Trust Proxy for Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  const logger = new Logger('Bootstrap');
+
+  // --- Get underlying Express adapter and set trust proxy ---
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
+  // -------------------------------------------------------
+
   const configService = app.get(ConfigService);
 
   // Read origins from environment variable, split by comma, default to empty array
